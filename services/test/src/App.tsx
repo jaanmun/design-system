86c239a5960112e64d4c8846faf13da63a3ce30d
JaanMun <yjm7575@naver.com>
2025-03-30 20:45:49 +0900

    feat: 컬러 토큰 만들기

---
+++ services/test/src/App.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import styled from '@emotion/styled';
+import { vars } from '@fastcampus/themes';
+
+function App() {
+  return <View />;
+}
+
+export default App;
+
+const View = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Text>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </Text>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+const Text = styled.p`
+  color: ${vars.colors.$static.light.gray[500]};
+`;
